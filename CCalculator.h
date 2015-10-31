4bd61fad4c60c394aaa62c9efae98314516041dd
Daria <dasham94@yandex.ru>
2015-11-01 00:39:29 +0300

    квадратное уравнение: косметик

--- CCalculator.h
+++ CCalculator.h
@@ -8,20 +8,8 @@
 class CCalculator {
 public:
 
-    //ax^2+bx+xc = d;
-    std::pair< std::complex <double>, std::complex <double> > quadraticEquation(double a, double b, double c, double d =0)
-    {
-        c = c - d;
-        if ( b*b - 4*a*c > 0) {
-            std::complex<double> x( ( -b + sqrt( b*b - 4*a*c ) ) / ( 2 * a ) );
-            std::complex<double> y( ( -b - sqrt( b*b - 4*a*c ) ) / ( 2 * a ) );
-            return std::make_pair< std::complex <double>, std::complex <double> >( x, y );
-        } else {
-            std::complex<double> x( -b / ( 2 * a ), ( sqrt( 4*a*c - b*b ) ) / ( 2 * a ) );
-            std::complex<double> y( -b / ( 2 * a ),  ( -sqrt( 4*a*c - b*b ) ) / ( 2 * a ) );
-            return std::make_pair< std::complex <double>, std::complex <double> >( x, y );
-        }
-    }
+    //ax^2 + bx + c = d;
+    static std::pair< std::complex <double>, std::complex <double> > quadraticEquation( double a, double b, double c, double d = 0 );
 };
 
 #endif // CCALCULATOR_H
