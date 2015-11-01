4e6ba7f28912197373a1010a0113a3f590c46f16
Daria <dasham94@yandex.ru>
2015-11-01 15:21:09 +0300

    СЛУ: заготовка

--- CCalculator.h
+++ CCalculator.h
@@ -3,13 +3,20 @@
 
 #include <cmath>
 #include <complex>
-
+#include <vector>
 
 class CCalculator {
+
 public:
 
     //ax^2 + bx + c = d;
     static std::pair< std::complex <double>, std::complex <double> > quadraticEquation( double a, double b, double c, double d = 0 );
+
+
+    // n - размерность матрицы; A[n][n] - матрица коэффициентов, F[n] - столбец свободных членов,
+    // X[n] - начальное приближение, ответ записывается также в X[n];
+    static void Jacobi (int n, const std::vector< std::vector <double> > & A, const std::vector<double>& F, std::vector<double>& X);
+
 };
 
 #endif // CCALCULATOR_H
--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@ using namespace std;
 
 int main()
 {
-    CCalculator calculator;
-    std::pair < std::complex< double >, std::complex< double > > result = calculator.quadraticEquation(1, 2, 1);
+    std::pair < std::complex< double >, std::complex< double > > result = CCalculator::quadraticEquation(1, 2, 1);
     cout << result.first.real() << " " << result.first.imag()<<std::endl;
     cout <<  result.second.real() << " " << result.second.imag()<<std::endl;
-    cout << "Hello World!" << endl;
+    int n = 3;
+    std::vector< std::vector<double> > A;
+    std::vector<double> F{1, 1, 1};
+    std::vector<double> X(n, 0);
+    A.push_back(std::vector<double>{1, 2, 3});
+    A.push_back(std::vector<double>{0, 1, 4});
+    A.push_back(std::vector<double>{0, 0, 1});
+    CCalculator::Jacobi(n, A, F, X);
+    for (int i = 0; i < n; ++i) {
+        std::cout << X[i]<<std::endl;
+    }
     return 0;
 }
 
